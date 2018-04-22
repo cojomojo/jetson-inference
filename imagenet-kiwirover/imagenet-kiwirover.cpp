589c139227ec5afd59cfb3c172cf0b77e2337046
Cody Balos <cjbalos@gmail.com>
2018-04-21 21:44:58 -0700

    code cleanup, and replace threaded image save with no thread save

--- imagenet-kiwirover/imagenet-kiwirover.cpp
+++ imagenet-kiwirover/imagenet-kiwirover.cpp
@@ -48,29 +48,29 @@
 #include "log.h"
 #include "XmlConfiguration.h"
 
-#define SLOW_DEMO_MODE 1
+#define SLOW_DEMO_MODE 0
 
 bool signal_received = false;
 void sig_handler(int signo);
 
 glDisplay* display = NULL;
 glTexture* texture = NULL;
-cudaFont* font = NULL;
+cudaFont*  font    = NULL;
 void create_opengl_display(uint32_t width, uint32_t height);
 void update_opengl_display(uint32_t width, uint32_t height, void* imgRGBA);
 
-int create_udp_socket(std::string host_name, struct sockaddr_in* servaddr);
+int create_udp_socket(std::string hostName, int hostPort, struct sockaddr_in* servaddr);
 bool send_trigger_over_udp(int socket, struct sockaddr_in* servaddr, uint8_t cam_index);
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
 	printf("imagenet-kiwirover\n  args (%i):  ", argc);
-	for( int i=0; i < argc; i++ )
+	for(int i=0; i < argc; i++)
 		printf("%i [%s]  ", i, argv[i]);
 	printf("\n\n");
 
 	// attach signal handler
-	if( signal(SIGINT, sig_handler) == SIG_ERR )
+	if (signal(SIGINT, sig_handler) == SIG_ERR)
 		printf("\ncan't catch SIGINT\n");
 
 	//
@@ -80,22 +80,23 @@ int main( int argc, char** argv )
 	// bool verbose = cli.GetFlag("v");
 	// bool xVerbose = cli.GetFlag("vv");
 	// bool xxVerbose = cli.GetFlag("vvv");
-	bool shouldDisplay = cli.GetFlag("display");
-	bool noUDPTrigger = cli.GetFlag("no-udp-trigger");
+	bool shouldDisplay  = cli.GetFlag("display");
+	bool noUDPTrigger   = cli.GetFlag("no-udp-trigger");
 	auto configFilename = cli.GetString("config-path") == NULL ? "config.xml" : cli.GetString("config-path");
 
 	//
 	// Parse configuration file.
 	//
 	XmlConfiguration xmlConfig(configFilename);
-	auto cam_serials = xmlConfig.GetStrings("cameras", "camera");
-	auto imWidth = xmlConfig.GetInt("cameras", "image_width");
-	auto imHeight = xmlConfig.GetInt("cameras", "image_height");
-	auto fps = xmlConfig.GetInt("cameras", "fps");
-	auto imBufferSize = xmlConfig.GetInt("imagebuffer");
-	auto host_name = xmlConfig.GetString("triggertarget");
-	auto saveCaptured = xmlConfig.GetBool("savecaptured");
-	auto capturedImageSavePath = xmlConfig.GetString("savecaptured", "path");
+	auto cam_serials   = xmlConfig.GetStrings("cameras", "camera");
+	auto imWidth       = xmlConfig.GetInt("cameras", "image_width");
+	auto imHeight      = xmlConfig.GetInt("cameras", "image_height");
+	auto fps           = xmlConfig.GetInt("cameras", "fps");
+	auto imBufferSize  = xmlConfig.GetInt("imagebuffer");
+	auto hostName      = xmlConfig.GetString("triggertarget");
+	auto hostPort      = xmlConfig.GetInt("triggertarget", "port");
+	auto saveCaptured  = xmlConfig.GetBool("savecaptured");
+	auto imageSavePath = xmlConfig.GetString("savecaptured", "path");
 
 	//
 	// Create the camera device
@@ -113,33 +114,27 @@ int main( int argc, char** argv )
 
 	// create the imageNet instance with the specified neural network
 	imageNet* net = imageNet::Create(argc, argv);
-	if( !net )
-	{
+	if (!net) {
 		printf("imagenet-kiwirover:   failed to initialize imageNet\n");
 		return 0;
 	}
 
 	// Create OpenGL stuff.
-	if( shouldDisplay )
-	{
+	if (shouldDisplay) {
 		create_opengl_display(camera->GetWidth(), camera->GetHeight());
 		font = cudaFont::Create();
 	}
 
 	int socket;
 	struct sockaddr_in servaddr;
-	if( !noUDPTrigger )
-	{
-		socket = create_udp_socket(host_name, &servaddr);
-		if( socket < 0 )
-		{
+	if (!noUDPTrigger) {
+		socket = create_udp_socket(hostName, hostPort, &servaddr);
+		if (socket < 0)
 			return 0;
-		}
 	}
 
 	// Start camera streaming.
-	if( !camera->Open() )
-	{
+	if (!camera->Open()) {
 		printf("\nimagenet-kiwirover:  failed to open camera for streaming\n");
 		return 0;
 	}
@@ -150,10 +145,9 @@ int main( int argc, char** argv )
 	 * processing loop
 	 */
 	uint32_t trigger_count = 0;
-	float confidence = 0.0f;
+	float    confidence    = 0.0f;
 
-	while( !signal_received )
-	{
+	while(!signal_received) {
 		void* imgCPU  = NULL;
 		void* imgCUDA = NULL;
 
@@ -172,7 +166,7 @@ int main( int argc, char** argv )
 		// classify image
 		const int img_class = net->Classify((float*)imgRGBA, camera->GetWidth(), camera->GetHeight(), &confidence);
 
-		if(img_class >= 0) {
+		if (img_class >= 0) {
 			const char* classDesc = net->GetClassDesc(img_class);
 			printf("imagenet-kiwirover:  %2.5f%% class #%i (%s)\n", confidence * 100.0f, img_class, classDesc);
 
@@ -183,8 +177,8 @@ int main( int argc, char** argv )
 
 			if (saveCaptured) {
 				uint32_t captureCount = camera->GetCaptureCount();
-				std::thread t([&]() {
-					std::string filename = capturedImageSavePath + classDesc + "_" + std::to_string(captureCount) + ".png";
+				//std::thread t([&]() {
+					std::string filename = imageSavePath + classDesc + "_" + std::to_string(captureCount) + ".png";
 					Pylon::CImagePersistence::Save(Pylon::EImageFileFormat::ImageFileFormat_Png,
 						filename.c_str(),
 						imgCPU,
@@ -193,11 +187,11 @@ int main( int argc, char** argv )
 						camera->GetWidth(),
 						camera->GetHeight(),
 						0,   // no padding
-						Pylon::EImageOrientation::ImageOrientation_TopDown
+						Pylon:: EImageOrientation:: ImageOrientation_TopDown
 					);
 					LOG_MSG(LOG_APP_LEVEL, "saved image " << captureCount << " to " << filename << std::endl);
-				});
-				t.detach();
+				//});
+				//t.detach();
 			}
 
 			if (shouldDisplay && (font != NULL)) {
@@ -225,28 +219,22 @@ int main( int argc, char** argv )
 	LOG_MSG(LOG_APP_LEVEL, "Sent " << trigger_count << " triggers." << std::endl);
 	printf("\nimagenet-kiwirover:  un-initializing video device\n");
 
-	// Shutdown the camera device.
-	if( camera != NULL )
-	{
-		delete camera;
-		camera = NULL;
-	}
-
 	//
 	// Clean up.
 	//
-	if( display != NULL )
-	{
+	if (camera != NULL) {
+		delete camera;
+		camera = NULL;
+	}
+	if (display != NULL) {
 		delete display;
 		display = NULL;
 	}
-	if( texture != NULL )
-	{
+	if (texture != NULL) {
 		delete texture;
 		texture = NULL;
 	}
-	if( font != NULL )
-	{
+	if (font != NULL) {
 		delete font;
 		font = NULL;
 	}
@@ -260,8 +248,7 @@ int main( int argc, char** argv )
 
 void sig_handler(int signo)
 {
-	if( signo == SIGINT )
-	{
+	if (signo == SIGINT) {
 		printf("received SIGINT\n");
 		signal_received = true;
 	}
@@ -276,13 +263,13 @@ void create_opengl_display(uint32_t width, uint32_t height)
 	display = glDisplay::Create();
 	texture = NULL;
 
-	if( !display ) {
+	if (!display) {
 		printf("\nimagenet-kiwirover:  failed to create openGL display\n");
 	}
 	else
 	{
 		texture = glTexture::Create(width, height, GL_RGBA32F_ARB/*GL_RGBA8*/);
-		if( !texture )
+		if (!texture)
 			printf("imagenet-kiwirover:  failed to create openGL texture\n");
 	}
 }
@@ -294,16 +281,13 @@ void create_opengl_display(uint32_t width, uint32_t height)
 void update_opengl_display(uint32_t width, uint32_t height, void* imgRGBA)
 {
 	// update display
-	if( display != NULL )
-	{
+	if (display != NULL) {
 		display->UserEvents();
 		display->BeginRender();
 
-		if( texture != NULL )
-		{
+		if (texture != NULL) {
 			// rescale image pixel intensities for display
-			CUDA
-			(
+			CUDA (
 				cudaNormalizeRGBA((float4*)imgRGBA, make_float2(0.0f, 255.0f),
 								  (float4*)imgRGBA, make_float2(0.0f, 1.0f),
 								  width, height)
@@ -312,8 +296,7 @@ void update_opengl_display(uint32_t width, uint32_t height, void* imgRGBA)
 			// map from CUDA to openGL using GL interop
 			void* tex_map = texture->MapCUDA();
 
-			if( tex_map != NULL )
-			{
+			if (tex_map != NULL) {
 				cudaMemcpy(tex_map, imgRGBA, texture->GetSize(), cudaMemcpyDeviceToDevice);
 				texture->Unmap();
 			}
@@ -327,12 +310,11 @@ void update_opengl_display(uint32_t width, uint32_t height, void* imgRGBA)
 }
 
 
-int create_udp_socket(std::string host_name, struct sockaddr_in* servaddr)
+int create_udp_socket(std::string hostName, int hostPort, struct sockaddr_in* servaddr)
 {
 	int soc;
 
-	if( (soc = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
-	{
+	if ((soc = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		LOG_ERROR(LOG_APP_LEVEL, "Cannot create UDP socket.\n");
 		return -1;
 	}
@@ -340,31 +322,31 @@ int create_udp_socket(std::string host_name, struct sockaddr_in* servaddr)
 	LOG_MSG(LOG_APP_LEVEL, "Created UDP socket.\n");
 
 	memset((char*) servaddr, 0, sizeof(*servaddr));
-	servaddr->sin_family = AF_INET;
+	servaddr->sin_family      = AF_INET;
 	servaddr->sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr->sin_port = htons(5005);
+	servaddr->sin_port        = htons(hostPort);
 
 	struct in_addr addr;
 
-	if (isalpha(host_name.c_str()[0])) {
-            printf("Calling gethostbyname with %s\n", host_name.c_str());
-            struct hostent* hp = gethostbyname(host_name.c_str());
-            LOG_MSG(LOG_APP_LEVEL, "Established UDP connection to " << host_name << std::endl);
+	if (isalpha(hostName.c_str()[0])) {
+		printf("Calling gethostbyname with %s\n", hostName.c_str());
+		struct hostent* hp = gethostbyname(hostName.c_str());
+		LOG_MSG(LOG_APP_LEVEL, "Established UDP connection to " << hostName << std::endl);
 	    memcpy((void*) &servaddr->sin_addr, hp->h_addr_list[0], hp->h_length);
 	} else {
-            printf("Calling gethostbyaddr with %s\n", host_name.c_str());
-            struct in_addr addr;
-	    addr.s_addr = inet_addr(host_name.c_str());
-            if (addr.s_addr == INADDR_NONE) {
-                printf("The IPv4 address entered must be a legal address\n");
-                return -1;
-            }
-            LOG_MSG(LOG_APP_LEVEL, "Established UDP connection to " << host_name << std::endl);
+		printf("Calling gethostbyaddr with %s\n", hostName.c_str());
+		struct in_addr addr;
+		addr.s_addr = inet_addr(hostName.c_str());
+		if (addr.s_addr == INADDR_NONE) {
+			printf("The IPv4 address entered must be a legal address\n");
+			return -1;
+		}
+		LOG_MSG(LOG_APP_LEVEL, "Established UDP connection to " << hostName << std::endl);
 	    memcpy((void*) &servaddr->sin_addr, &addr, sizeof(addr));
-        }
+	}
 
 	//if (!hp) {
-	//	LOG_ERROR(LOG_APP_LEVEL, "Could not obtain address of " << host_name << std::endl);
+	//	LOG_ERROR(LOG_APP_LEVEL, "Could not obtain address of " << hostName << std::endl);
 	//	return -1;
 	//}
 
